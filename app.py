c0d71108b379dc708fc26cd2ca9357bafb8bc7e1
Brian-15 <bcirieco@gmail.com>
2021-09-19 20:12:30 -0700

    implemented /logout route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import pdb
 
 from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
@@ -113,7 +114,10 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
+
+    flash("Successfully logged out.", "success")
+    return redirect("/login")
 
 
 ##############################################################################
